4cc7c4e7534b80df361c00ac4bb063e22f3f4885
Vincent Vielle <viellevincentd+github@gmail.com>
2024-02-18 14:53:15 +0100

    final tests

--- src/internal/service/v1/users_test.go
+++ src/internal/service/v1/users_test.go
@@ -8,6 +8,7 @@ import (
 	user_store_svc_v1_entities "github.com/Golerplate/contracts/clients/user-store-svc/v1/entities"
 	user_store_svc_v1_mocks "github.com/Golerplate/contracts/clients/user-store-svc/v1/mocks"
 	"github.com/Golerplate/pkg/constants"
+	pkgerrors "github.com/Golerplate/pkg/errors"
 	"github.com/stretchr/testify/assert"
 	"go.uber.org/mock/gomock"
 
@@ -87,11 +88,52 @@ func Test_GetUserByIdentifier(t *testing.T) {
 	})
 
 	t.Run("nok - user not found by id", func(t *testing.T) {
+		ctrl := gomock.NewController(t)
+		m := user_store_svc_v1_mocks.NewMockUserStoreSvc(ctrl)
+
+		userid := constants.GenerateDataPrefixWithULID(constants.User)
+
+		m.EXPECT().GetUserByID(gomock.Any(), userid).Return(nil, pkgerrors.NewNotFoundError("not found"))
+
+		s, err := service_v1.NewService(context.Background(), m)
+		assert.NotNil(t, s)
+		assert.NoError(t, err)
+
+		user, err := s.GetUserByIdentifier(context.Background(), userid)
+		assert.Nil(t, user)
+		assert.Error(t, err)
+		assert.True(t, pkgerrors.IsNotFoundError(err))
 	})
 
 	t.Run("nok - user not found by username", func(t *testing.T) {
+		ctrl := gomock.NewController(t)
+		m := user_store_svc_v1_mocks.NewMockUserStoreSvc(ctrl)
+
+		m.EXPECT().GetUserByUsername(gomock.Any(), "testuser").Return(nil, pkgerrors.NewNotFoundError("not found"))
+
+		s, err := service_v1.NewService(context.Background(), m)
+		assert.NotNil(t, s)
+		assert.NoError(t, err)
+
+		user, err := s.GetUserByIdentifier(context.Background(), "testuser")
+		assert.Nil(t, user)
+		assert.Error(t, err)
+		assert.True(t, pkgerrors.IsNotFoundError(err))
 	})
 
-	t.Run("client error", func(t *testing.T) {
+	t.Run("nok - client error", func(t *testing.T) {
+		ctrl := gomock.NewController(t)
+		m := user_store_svc_v1_mocks.NewMockUserStoreSvc(ctrl)
+
+		m.EXPECT().GetUserByUsername(gomock.Any(), "testuser").Return(nil, pkgerrors.NewInternalServerError("failed"))
+
+		s, err := service_v1.NewService(context.Background(), m)
+		assert.NotNil(t, s)
+		assert.NoError(t, err)
+
+		user, err := s.GetUserByIdentifier(context.Background(), "testuser")
+		assert.Nil(t, user)
+		assert.Error(t, err)
+		assert.True(t, pkgerrors.IsInternalServerError(err))
 	})
 }
